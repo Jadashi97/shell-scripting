05d4aaa6cf4f28e6eaa77ba235e363e3bcf162d1
jadashi97 <njadauniversal23@yahoo.com>
2022-08-03 10:48:56 -0500

    (feat): learn user input, if statements and variables

--- myscript.sh
+++ myscript.sh
@@ -1,11 +1,53 @@
-#! /bin/zsh
+#!/bin/bash
+
+
 
 #ECHO COMMAND
-echo Yaaa Manyang!
+# echo Yaaa Manyang!
 
 # VARIABLES
-# Uppercase by convention
+# Uppercase by convention 
 # Letters, numbers, underscores
 
-# NAME = "Daniel"
-# echo "My name is $NAME"
+NAME="jabawoki"
+# echo "My name is, ${NAME}"
+
+
+#USER INPUT
+# read -p "Enter your name: " NAME
+# echo "Hello $NAME, nice to meet you!"
+
+
+#SIMPLE IF STATEMENT
+# if [ "${NAME}" == "Daniel" ]
+# then
+# echo "Your name is Daniel"
+# fi
+
+# IF-ELSE 
+# if [ "${NAME}" == "Daniel" ]
+# then
+#     echo "You are the village Chief!!"
+# else
+#     echo "Come on you are not the village chief!!"
+# fi
+
+#ELSE-IF (elif)
+# if [ "${NAME}" == "Daniel" ]
+# then
+#     echo "You are the real village Chief!!"
+# elif [ "${NAME}" == "Emman" ]
+# then
+#     echo "You are the old and former village Chief!!"
+# else
+#     echo "Come on you are not and have never been the village chief!!"
+# fi
+
+###############
+# val1 -eq val2 Returns  true if the values are equal
+# val1 -ne val2 Returns true if the values are not equal
+# val1 -gt val2 Returns true if val1 > val2
+# val1 -ge val2 Returns true if val1 >= val2
+# val1 -lt val2 Returns true if val1 < val2
+# val1 -le val2 Returns true if val1 <= val2
+############### 
