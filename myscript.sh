bfa3447039bc4734d1a5f6e55ee46ab2f3dcece1
jadashi97 <njadauniversal23@yahoo.com>
2022-08-05 10:48:08 -0500

    (feat): practice shell scritping. add folder and new file

--- myscript.sh
+++ myscript.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 
 
@@ -10,147 +10,156 @@
 # Letters, numbers, underscores
 
 # NAME="jabawoki"
-echo "My name is, ${NAME}"
+# echo "My name is, ${NAME}"
 
 
-#USER INPUT
-read -p "Enter your name: " NAME
-echo "Hello $NAME, nice to meet you!"
+# #USER INPUT
+# read -p "Enter your name: " NAME
+# echo "Hello $NAME, nice to meet you!"
 
 
-#SIMPLE IF STATEMENT
-if [ "${NAME}" == "Daniel" ]
-then
-echo "Your name is Daniel"
-fi
+# #SIMPLE IF STATEMENT
+# if [ "${NAME}" == "Daniel" ]
+# then
+# echo "Your name is Daniel"
+# fi
 
-# IF-ELSE 
-if [ "${NAME}" == "Daniel" ]
-then
-    echo "You are the village Chief!!"
-else
-    echo "Come on you are not the village chief!!"
-fi
+# # IF-ELSE 
+# if [ "${NAME}" == "Daniel" ]
+# then
+#     echo "You are the village Chief!!"
+# else
+#     echo "Come on you are not the village chief!!"
+# fi
 
-#ELSE-IF (elif)
-if [ "${NAME}" == "Daniel" ]
-then
-    echo "You are the real village Chief!!"
-elif [ "${NAME}" == "Emman" ]
-then
-    echo "You are the old and former village Chief!!"
-else
-    echo "Come on you are not and have never been the village chief!!"
-fi
+# #ELSE-IF (elif)
+# if [ "${NAME}" == "Daniel" ]
+# then
+#     echo "You are the real village Chief!!"
+# elif [ "${NAME}" == "Emman" ]
+# then
+#     echo "You are the old and former village Chief!!"
+# else
+#     echo "Come on you are not and have never been the village chief!!"
+# fi
 
-###############
-# val1 -eq val2 Returns  true if the values are equal
-# val1 -ne val2 Returns true if the values are not equal
-# val1 -gt val2 Returns true if val1 > val2
-# val1 -ge val2 Returns true if val1 >= val2
-# val1 -lt val2 Returns true if val1 < val2
-# val1 -le val2 Returns true if val1 <= val2
-############### 
+# ###############
+# # val1 -eq val2 Returns  true if the values are equal
+# # val1 -ne val2 Returns true if the values are not equal
+# # val1 -gt val2 Returns true if val1 > val2
+# # val1 -ge val2 Returns true if val1 >= val2
+# # val1 -lt val2 Returns true if val1 < val2
+# # val1 -le val2 Returns true if val1 <= val2
+# ############### 
 
-NUM1=20
-NUM2=20
+# NUM1=20
+# NUM2=20
 
-if [ "${NUM1}" -gt "${NUM2}" ]
-then
-    echo "i think you nailed the whole thing!!"
-elif [ "${NUM1}" -eq "${NUM2}" ]
-then
-    echo "please try again dummy!!"
-fi
+# if [ "${NUM1}" -gt "${NUM2}" ]
+# then
+#     echo "i think you nailed the whole thing!!"
+# elif [ "${NUM1}" -eq "${NUM2}" ]
+# then
+#     echo "please try again dummy!!"
+# fi
 
-###################
-# -d file =True if the file is a directory 
-# -e file =True if the file exists (note that this is not particularly portable, thus -f is generally used)
-# -f file =True if the provided string is a file
-# -g file =True if the group id is set on a file
-# -r file =True if the file is readable
-# -s file =True if the file has a non zero size
-# -u file = True if the user id is set on a file
-# -w file =True if the file is writeable
-# -x file =True if the file is an executable one
-###################
-
-
-# FILE CONDITIONS
-FILE="test.text"
-
-if [ -d "${FILE}" ]
-then
-    echo "I see the file ${FILE}"
-else
-    echo "Naahh you must be dreaming!!"
-fi
+# ###################
+# # -d file =True if the file is a directory 
+# # -e file =True if the file exists (note that this is not particularly portable, thus -f is generally used)
+# # -f file =True if the provided string is a file
+# # -g file =True if the group id is set on a file
+# # -r file =True if the file is readable
+# # -s file =True if the file has a non zero size
+# # -u file = True if the user id is set on a file
+# # -w file =True if the file is writeable
+# # -x file =True if the file is an executable one
+# ###################
 
 
-# CASE STATEMENTS WHILE USING USER INPUT
-read -p "Are you 21 or over? Y/N" ANSWER
+# # FILE CONDITIONS
+# FILE="test.text"
 
-# case syntax
-case "${ANSWER}" in 
-    [Yy] | [Yy][eE][sS])
-        echo "You can grab a beer :)"
-        ;;
-    [nN] | [nN][oO])
-        echo "Sorry, No drinking for you my friend"
-        ;;
-    *)
-        echo "Please enter y/yes or no/no" #this is the default case as in js case-switch
-        ;;
-esac #closes out the the case 
-
-#SIMPLE FOR LOOP 
-NAMES="Lokose Daniel Ronald Stephen Winnie Jimmy Toni Tom"
-for NAME in $NAMES
-do
-    echo "Hello $NAME"
-done
-
-# FOR LOOP TO RENAME FILE (problem solved if you have a huge file that needs renaming)
-FILES=$(ls *.text)
-NEW="new"
-for FILE in ${FILES}
-    do
-        echo "Renaming ${FILE} to new-${FILE}"
-        mv ${FILE} ${NEW}-${FILE}
-done
+# if [ -d "${FILE}" ]
+# then
+#     echo "I see the file ${FILE}"
+# else
+#     echo "Naahh you must be dreaming!!"
+# fi
 
 
-# WHILE LOOP TO - READ THROUGH  A FILE LINE BY LINE
+# # CASE STATEMENTS WHILE USING USER INPUT
+# read -p "Are you 21 or over? Y/N" ANSWER
 
-LINE=1
-while read -r CURRENT_LINE
-    do
-        echo "$LINE: $CURRENT_LINE"
-        ((LINE++))
-done < "./new-test1.text"
+# # case syntax
+# case "${ANSWER}" in 
+#     [Yy] | [Yy][eE][sS])
+#         echo "You can grab a beer :)"
+#         ;;
+#     [nN] | [nN][oO])
+#         echo "Sorry, No drinking for you my friend"
+#         ;;
+#     *)
+#         echo "Please enter y/yes or no/no" #this is the default case as in js case-switch
+#         ;;
+# esac #closes out the the case 
+
+# #SIMPLE FOR LOOP 
+# NAMES="Lokose Daniel Ronald Stephen Winnie Jimmy Toni Tom"
+# for NAME in $NAMES
+# do
+#     echo "Hello $NAME"
+# done
+
+# # FOR LOOP TO RENAME FILE (problem solved if you have a huge file that needs renaming)
+# FILES=$(ls *.text)
+# NEW="new"
+# for FILE in ${FILES}
+#     do
+#         echo "Renaming ${FILE} to new-${FILE}"
+#         mv ${FILE} ${NEW}-${FILE}
+# done
 
 
-# FUNCTIONS
-function sayMayang(){
-    echo "yaaaasalam!! Manyang get outta here!"
-}
+# # WHILE LOOP TO - READ THROUGH  A FILE LINE BY LINE
 
-sayMayang
-
-# FUNCTIONS WITH PARAMS
-
-function wazzzzaa(){
-    echo "ya zol!! I am $1 and I am on this $2 path forever untill i see and exceed $3 $4"
-}
-
-wazzzzaa "broke" "developer" "success" "PERIOD"
+# LINE=1
+# while read -r CURRENT_LINE
+#     do
+#         echo "$LINE: $CURRENT_LINE"
+#         ((LINE++))
+# done < "./new-test1.text"
 
 
-# USE-CASE TO SOLVE ANY PROBLEM OF DOING REPETITIVE WORK OVER AND OVER
+# # FUNCTIONS
+# function sayMayang(){
+#     echo "yaaaasalam!! Manyang get outta here!"
+# }
 
-# PROBLEM - CREATE A FOLDER AND WRITE TO A FILE
-mkdir hello
+# sayMayang
 
-touch "hello/dunia.txt"
-echo "Sallam dunia and nas bet" >> "hello/dunia.txt"
-echo "Created salllam/dunia.txt"
+# # FUNCTIONS WITH PARAMS
+
+# function wazzzzaa(){
+#     echo "ya zol!! I am $1 and I am on this $2 path forever untill i see and exceed $3 $4"
+# }
+
+# wazzzzaa "broke" "developer" "success" "PERIOD"
+
+
+## USE-CASE TO SOLVE ANY PROBLEM OF DOING REPETITIVE WORK OVER AND OVER
+
+# # PROBLEM - CREATE A FOLDER AND WRITE TO A FILE
+# mkdir hello
+
+# touch "hello/dunia.txt"
+# echo "Sallam dunia and nas bet" >> "hello/dunia.txt"
+# echo "Created salllam/dunia.txt"\
+
+# example 2 - CREATE A FILE TO HOLD PLUGIN REF COMMANDS TO USE IN THE TERMINAL WITH ZSH
+
+# mkdir plugins
+# touch "plugins/zsh-pluggins.txt"
+echo "main pluggins web-search, dirhistory, zsh_reload sudo" >> "plugins/zsh-pluggins.txt"
+echo "Created pluggins for my zsh file"
+
+
